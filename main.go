a5f10e17242bce94e2289e42812116d7130ccafe
SeongHyun <abc7468@naver.com>
2022-07-24 04:36:02 +0900

    feat : Gin 프레임워크를 사용하여 List, Add 기능 웹 상에 출력

--- main.go
+++ main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"coin/blockchain"
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
+const port string = ":4000"
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func showHomePage(c *gin.Context) {
+	data := homeData{"roy coin", blockchain.GetBlockchain().AllBlocks()}
+	c.HTML(http.StatusOK, "home", data)
+}
+func showAddPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "add", nil)
+}
+func addBlock(c *gin.Context) {
+	c.Request.ParseForm()
+	data := c.PostForm("data")
+	blockchain.GetBlockchain().AddBlock(data)
+	c.Redirect(308, "http://localhost"+port)
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
-	}
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*")
+	r.GET("/", showHomePage)
+	r.GET("/add", showAddPage)
+	r.POST("/add", addBlock)
+	r.Run(port)
 }
