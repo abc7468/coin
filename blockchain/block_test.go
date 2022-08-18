82a9a86a47350370ac5590b1ec358338b10a4e05
abc7468 <abc7468@naver.com>
2022-08-19 01:56:00 +0900

    #13.14

--- blockchain/block_test.go
+++ blockchain/block_test.go
@@ -1,14 +1,45 @@
 package blockchain
 
 import (
+	"coin/utils"
 	"reflect"
 	"testing"
 )
 
 func TestCreateBlock(t *testing.T) {
 	dbStorage = fakeDB{}
+	Mempool().Txs["test"] = &Tx{}
 	b := createBlock("x", 1, 1)
 	if reflect.TypeOf(b) != reflect.TypeOf(&Block{}) {
 		t.Error("createBlock() should return an instance of a block")
 	}
+}
+
+func TestFindBlock(t *testing.T) {
+	t.Run("Block not found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				return nil
+			},
+		}
+		_, err := FindBlock("xx")
+		if err == nil {
+			t.Error("The block should not be found!")
+		}
+	})
+	t.Run("Block is found", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeFindBlock: func() []byte {
+				b := &Block{}
+				return utils.ToBytes(b)
+			},
+		}
+		b, err := FindBlock("xx")
+		if err != nil {
+			t.Error("The block should be found")
+		}
+		if reflect.TypeOf(b) != reflect.TypeOf(&Block{}) {
+			t.Error("The block should be found")
+		}
+	})
 }
--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -1,5 +1,12 @@
 package blockchain
 
+import (
+	"coin/utils"
+	"reflect"
+	"sync"
+	"testing"
+)
+
 type fakeDB struct {
 	fakeLoadChain func() []byte
 	fakeFindBlock func() []byte
@@ -19,4 +26,36 @@ func (f fakeDB) LoadChain() []byte {
 }
 func (fakeDB) DeleteAllBlocks() {
 	return
+}
+
+func TestBlockchain(t *testing.T) {
+	t.Run("불러올 블록체인 없을 때", func(t *testing.T) {
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				return nil
+			},
+		}
+		bc := Blockchain()
+		if bc.Height != 1 {
+			t.Error("Height가 1인 블록체인이 만들어 져야합니다.")
+		}
+	})
+	t.Run("불러올 블록체인 있을 때", func(t *testing.T) {
+		once = sync.Once{}
+		dbStorage = fakeDB{
+			fakeLoadChain: func() []byte {
+				b := &blockchain{
+					Height: 2,
+				}
+				return utils.ToBytes(b)
+			},
+		}
+		bc := Blockchain()
+		if bc.Height != 2 {
+			t.Errorf("Height가 %d인 블록체인이 불려져야 하지만, Height가 %d인 블록체인이 불려집니다.", 2, bc.Height)
+		}
+		if reflect.TypeOf(bc) != reflect.TypeOf(&blockchain{}) {
+			t.Error("Blockchain()이 반환해야 하는 타입이 옳지 않습니다.")
+		}
+	})
 }
